ca916a087e42acc7a0cf62342581738eaa734ab5
Riku-Juhani Niemela <rjniem@gmail.com>
2019-11-03 13:15:34 +0200

    update our menu-items to navigate withRouter HOC

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, subtitle, size }) => {
+const MenuItem = ({ title, imageUrl, subtitle, size, history, linkUrl, match }) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className='background-image'
                 style={{
                     backgroundImage: `url(${imageUrl})`
@@ -29,4 +31,4 @@ MenuItem.propTypes = {
     subtitle: PropTypes.string.isRequired
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'));
