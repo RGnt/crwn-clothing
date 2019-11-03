b999a804f94f10f91496ef67b2e02bae2d5e7826
Riku-Juhani Niemela <rjniem@gmail.com>
2019-11-03 13:58:37 +0200

    So many changes

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -6,13 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
 
 ReactDOM.render(
     <BrowserRouter>
